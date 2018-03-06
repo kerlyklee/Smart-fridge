4d35d4d6af17480c5cbe288ee51460eaa2c38a01
Unknown <keklee@ttu.ee>
2018-03-06 20:49:19 +0200

    Vormi väärtuste sidumine

--- tarkKulmik/src/foodTable/foodTable.js
+++ tarkKulmik/src/foodTable/foodTable.js
@@ -1,5 +1,33 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class foodTable {
+	userData = {}
+	userList = []
+
   constructor() {
-    
+  	
+   
   }
+  activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/users')
+			.then(response => response.json())
+			.then(users => this.userList = users);
+	}
+
+	addUser() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/users/add', {
+			'method': "POST",
+			'body': json(this.userData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatis " + data.foodName);
+		});
+
+		console.log("Method executed!")
+	}
 }
