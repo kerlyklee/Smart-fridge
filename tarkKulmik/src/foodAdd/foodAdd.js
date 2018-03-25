f9dafedd277a7a4d134925adb6bea79ced261323
danaju <36476033+danaju@users.noreply.github.com>
2018-03-25 17:07:15 +0300

    Kujundus paremaks

--- tarkKulmik/src/foodAdd/foodAdd.js
+++ tarkKulmik/src/foodAdd/foodAdd.js
@@ -1,22 +1,25 @@
-import {HttpClient, json} from 'aurelia-fetch-client'
+import {
+  HttpClient,
+  json
+} from 'aurelia-fetch-client'
 
 export class foodAdd {
 
-  	addFood() {
-		let client = new HttpClient();
- 		let foodData = {}
+  addFood() {
+    let client = new HttpClient();
+    let foodData = {}
 
-		client.fetch('http://localhost:8080/foods/add', {
-      'method': 'POST',
-      'body': json(this.foodData)
-    })
-			.then(response => response.json())
-			.then(data => {
-				console.log('Server saatis ' + data.foodName);
-			});
-			.catch(error => {
-        console.log("Error!" + error.message)
+    client.fetch('http://localhost:8080/foods/add', {
+        'method': 'POST',
+        'body': json(this.foodData)
       })
-			console.log("Method executed")
-		}
-	}
+      .then(response => response.json())
+      .then(data => {
+        console.log('Server saatis ' + data.foodName);
+      })
+    .catch(error => {
+      console.log("Error!" + error.message);
+    });
+    console.log("Method executed")
+  }
+}
