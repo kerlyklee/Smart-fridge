556ca29c83f2d951cb676a08d933c5d3a9eb2ece
danaju <36476033+danaju@users.noreply.github.com>
2018-03-28 19:16:00 +0300

    Tabel kuvab andmebaasist toidud

--- tarkKulmik/src/foodOverview/foodOverview.js
+++ tarkKulmik/src/foodOverview/foodOverview.js
@@ -1,3 +1,13 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
 export class foodOverview {
 
-}
+  foodlist=[]
+
+  activate() {
+
+     let client = new HttpClient();
+     client.fetch('http://localhost:8080/foods')
+       .then(response => response.json())
+       .then(foods => this.foodList = foods);
+   }
+ }
